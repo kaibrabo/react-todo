6d14c402b8a8cff2bd03d978e83804066a1a7f5e
Kai Brabo <kaibrabo@gmail.com>
2017-12-05 01:34:23 -0800

    state & props

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 class ToDo extends Component {
     render() {
         return (
-            <li>Return a List Item Here</li>
+            <li>
+                <input type="checkbox" checked={ this.props.isCompleted } />
+                <span>{ this.props.description }</span>
+            </li>
         );
     }
 }
